6e774809efaf66a38a3de119cc81137bcb51c465
Lars Sperling <155863133+SLworld13@users.noreply.github.com.>
2024-12-04 12:55:21 +0100

    added fetch, now we can see the list

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -1,8 +1,8 @@
 export default function ArtPiecePreview({ piece }) {
   return (
-    <div>
-      <img>{piece.imageSource}</img>
-      <h2>{piece.artist}</h2>
+    <div className="art-piece-preview">
+      <h3>{piece.artist}</h3>
+      <img src={piece.imageSource} alt={piece.name} height="300" width="300" />
       <p>{piece.name}</p>
     </div>
   );
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,18 +1,29 @@
 import ArtPieceList from "../components/ArtPieceList";
+import useSWR from "swr";
 
-export default function GalleryPage({ artPieces }) {
-  return (
-    <div>
-      <h1>GALLERY</h1>
-      <ArtPieceList artPieces={artPieces}></ArtPieceList>
-    </div>
-  );
-}
-
+// fetcher-Funktion
 export async function fetcher(url) {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch data");
   }
   return response.json();
+}
+
+export default function GalleryPage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  console.log("Fetched data:", data);
+
+  if (error) return <div>Error loading data...</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h1>GALLERY</h1>
+      <ArtPieceList artPieces={data} />
+    </div>
+  );
 }
